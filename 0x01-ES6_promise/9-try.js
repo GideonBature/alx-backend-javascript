c5bf730c0337b9cda49da22f0e4a0448bab47e84
GideonBature <infoaboutgideon@gmail.com>
2024-03-06 17:45:51 +0100

    add Error: typo

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(`${error.message}`);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
