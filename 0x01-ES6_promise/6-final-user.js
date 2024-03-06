034c3fffd648aa5ef21fdc96e4d713c95c44b63b
GideonBature <infoaboutgideon@gmail.com>
2024-03-06 11:59:53 +0100

    refactor the code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => {
+      const res = [];
       if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
+        res.push({ status: result.status, value: result.value });
+      } else {
+        res.push({ status: result.status, value: result.reason.toString() });
       }
-      return { status: result.status, value: result.reason.toString() };
+      return res;
     }));
 }
