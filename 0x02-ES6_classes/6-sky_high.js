da4573a45c6df878d9448156f340f7b25be12329
GideonBature <infoaboutgideon@gmail.com>
2024-03-07 22:42:52 +0100

    implemented inheritance and override the method in the super class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
