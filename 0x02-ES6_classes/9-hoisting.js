ed13033515ffc16af68d20e9538f521859bf0c62
GideonBature <infoaboutgideon@gmail.com>
2024-03-08 03:26:06 +0100

    fix typo

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -28,7 +28,7 @@ export class StudentHolberton {
     return this._holbertonClass;
   }
 
-  fullStudentDescription() {
+  get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
