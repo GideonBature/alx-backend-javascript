5c7eb4b302058c313b977fb9a46cb02a1b9e66d1
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 20:44:13 +0100

    create map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const map = new Map();
+
+  return map
+    .set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+}
