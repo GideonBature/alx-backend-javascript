dd3ccbe4977e7d02b9f39cc5f407f4706ef73a77
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 23:51:42 +0100

    re-arrange code

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const accessCount = weakMap.get(endpoint) || 0;
-
-  if (accessCount >= 5) throw new Error('Endpoint load is high');
-
+  let accessCount = weakMap.get(endpoint) || 0;
+  
   weakMap.set(endpoint, accessCount + 1);
+
+  if (accessCount + 1 >= 5) throw new Error('Endpoint load is high');
+
 
   return weakMap.get(endpoint);
 }
