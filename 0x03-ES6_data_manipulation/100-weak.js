88a9b22d737d83395216dc5e883ad555f96ce216
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 23:52:59 +0100

    fix eslint style

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let accessCount = weakMap.get(endpoint) || 0;
-  
+  const accessCount = weakMap.get(endpoint) || 0;
+
   weakMap.set(endpoint, accessCount + 1);
 
   if (accessCount + 1 >= 5) throw new Error('Endpoint load is high');
-
 
   return weakMap.get(endpoint);
 }
