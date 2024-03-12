bdc704a7cd2550705f70814bfc3ff4650c9286cf
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 11:13:18 +0100

    returns ArrayBuffer with an Int8 value at specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    return new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const int8Array = new Int8Array(buffer);
+
+  // int8Array.set(value, position)
+  int8Array[position] = value;
+
+  const dataView = new DataView(buffer);
+
+  return dataView;
+}
