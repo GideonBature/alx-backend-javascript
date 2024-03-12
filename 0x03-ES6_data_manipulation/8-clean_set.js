8d1bfb96e0582d76604c6c1483feb909fa8b7be6
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 17:41:49 +0100

    improve code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,11 @@
 export default function cleanSet(set, startString) {
-  let word = '';
+  if (!startString) return '';
 
-  if (startString === '') return '';
-
+  const result = [];
   for (const str of set) {
     if (str.startsWith(startString)) {
-      const newStr = str.slice(startString.length, str.length + 1);
-
-      if (word.length > 1) word += '-';
-
-      word += newStr;
+      result.push(str.slice(startString.length));
     }
   }
-  return word;
+  return result.join('-');
 }
