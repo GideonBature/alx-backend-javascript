cce915330d56ff693e3495061fb948ff964106b0
GideonBature <infoaboutgideon@gmail.com>
2024-03-12 11:17:30 +0100

    correct return to throw

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    return new Error('Position outside range');
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
