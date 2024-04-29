05bf70f9c1cd575b764f384b36220881f8fabc92
GideonBature <infoaboutgideon@gmail.com>
2024-04-29 22:34:30 +0100

    a program named 1-stdin.js that will be executed through command line

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("data", (data) => {
+	process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on("end", () => {
+	process.stdout.write("This important software is now closing\n");
+});
