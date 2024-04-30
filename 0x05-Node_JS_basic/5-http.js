7046f48cb948bda00f131e02db8d71227833b77d
GideonBature <infoaboutgideon@gmail.com>
2024-04-30 06:20:55 +0100

    Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const countStudents = require('./3-read_file_async');
+
+const file = process.argv[2];
+
+const app = http.createServer(async(req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    return res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(file);
+      return res.end(`${students.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
+    }
+  }
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
