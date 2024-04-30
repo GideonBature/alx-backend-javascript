3570603453541e4d71365ade4d0b2815093cae56
GideonBature <infoaboutgideon@gmail.com>
2024-04-30 10:33:15 +0100

    fix code

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,20 +5,20 @@ const countStudents = require('./3-read_file_async');
 const file = process.argv[2];
 
 const app = http.createServer(async(req, res) => {
-  res.writeHead('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.write('Hello Holberton School!');
+    return res.end();
   }
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
       const students = await countStudents(file);
-      res.write(`${students}`);
+      return res.end(`${students.join('\n')}`);
     } catch (err) {
-      return res.end(err.message);
+      res.end(err.message);
     }
   }
-  return res.end();
+  res.end();
 });
 
 app.listen(1245);
