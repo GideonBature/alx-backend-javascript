9fd799e86f9cb25788654a2e352f6c72b4ca3696
GideonBature <infoaboutgideon@gmail.com>
2024-05-02 19:06:17 +0100

    fix code

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -8,6 +8,10 @@ describe('Testing numbers', () => {
 	it('2 is equal to 2', () => {
 		expect(2 === 2).to.be.true;
 	});
+
+	it.skip('1 is equal to 3', () => {
+		expect(1 === 3).to.be.true;
+	});
 	
 	it('3 is equal to 3', () => {
 		expect(3 === 3).to.be.true;
