9e5103592f89fcadbb59cbe02af332a57c6fbbd9
GideonBature <infoaboutgideon@gmail.com>
2024-05-02 19:23:43 +0100

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+    res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = (link unavailable);
-  res.send(`Payment methods for cart ${id}`);
+// Add a new endpoint for /cart/:id where :id must be a number
+app.get('/cart/:id', (req, res) => {
+    const { id } = req.params;
+
+    // Check if id is a number using a regular expression
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).send('Invalid cart ID, must be a number');
+    }
+
+    // Respond with a message incorporating the cart ID
+    res.send(`Payment methods for cart ${id}`);
 });
 
 app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+    console.log('API is running on port 7865');
 });
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,33 +2,48 @@ const request = require('request');
 const expect = require('chai').expect;
 
 describe('Index page', function() {
-  it('should return a 200 status code', function(done) {
-    request.get('http://localhost:7865/', function(err, res, body) {
-      expect(res.statusCode).to.equal(200);
-      done();
+    it('should return a 200 status code', function(done) {
+        request.get('http://localhost:7865/', function(err, res, body) {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
     });
-  });
 
-  it('should return the correct result', function(done) {
-    request.get('http://localhost:7865/', function(err, res, body) {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+    it('should return the correct result', function(done) {
+        request.get('http://localhost:7865/', function(err, res, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
     });
-  });
 });
 
+// New test suite for the /cart/:id endpoint
 describe('Cart page', function() {
-  it('should return a 200 status code when :id is a number', function(done) {
-    request.get('http://localhost:7865/cart/123', function(err, res, body) {
-      expect(res.statusCode).to.equal(200);
-      done();
+    it('should return a 200 status code for a valid numeric id', function(done) {
+        request.get('http://localhost:7865/cart/123', function(err, res, body) {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
     });
-  });
 
-  it('should return a 404 status code when :id is not a number', function(done) {
-    request.get('http://localhost:7865/cart/abc', function(err, res, body) {
-      expect(res.statusCode).to.equal(404);
-      done();
+    it('should return the correct response for a valid numeric id', function(done) {
+        request.get('http://localhost:7865/cart/123', function(err, res, body) {
+            expect(body).to.include('Payment methods for cart 123');
+            done();
+        });
     });
-  });
+
+    it('should return a 400 status code for an invalid id (non-numeric)', function(done) {
+        request.get('http://localhost:7865/cart/abc', function(err, res, body) {
+            expect(res.statusCode).to.equal(400);
+            done();
+        });
+    });
+
+    it('should return the correct error message for an invalid id', function(done) {
+        request.get('http://localhost:7865/cart/abc', function(err, res, body) {
+            expect(body).to.equal('Invalid cart ID, must be a number');
+            done();
+        });
+    });
 });
