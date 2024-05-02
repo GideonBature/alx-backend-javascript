ec7dab2516b9aafdb8d52f44e2280f406f454ea4
GideonBature <infoaboutgideon@gmail.com>
2024-05-02 07:04:56 +0100

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,7 +1,6 @@
-const chai = await import('chai');
-const expect = chai.expect;
-
 const calculateNumber = require('./2-calcul_chai');
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('calculateNumber', () => {
 	// SUM
